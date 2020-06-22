8f0f99aa991e02ae954aec8624108385ec10b14c
flyonly <fourshorts@gmail.com>
2020-06-23 06:28:46 +1000

    Changed servo driver.  Old one did seem to work with DS6150.  Move buttons around

--- ATtiny85_Shifter.ino
+++ ATtiny85_Shifter.ino
@@ -4,11 +4,18 @@
 // will go down one gear then back up half a gear to get to netural.  So this should
 // work from 1st or 2nd.  Havent yet tried it on a motorcycle.  It will then show
 // green to indicate you are in netrual
+//   pinMode(1, OUTPUT); //LED on Model A  or Pro
 
-#include <SoftwareServo.h>        //needs to be 8Mhz
+#include <SoftRcPulseOut.h> 
 #include <Adafruit_NeoPixel.h>
 
-SoftwareServo servo;              // Define servo
+SoftRcPulseOut myservo;  // create servo object to control a servo 
+                        // a maximum of eight servo objects can be created 
+                        
+#define SERVO_PIN      3
+
+#define REFRESH_PERIOD_MS 3
+#define NOW               1
 
 #define PIN            4          // NeoPixel pin number
 #define NUMPIXELS      8          // NeoPixel Pixels
@@ -16,8 +23,8 @@ SoftwareServo servo;              // Define servo
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 #define ACTIVATED LOW
-const int buttonUp = 1;           // the number of the pushbutton pin 1
-const int buttonDown = 2;         // the number of the pushbutton pin 2
+const int buttonUp = 2;           // the number of the pushbutton pin 2
+const int buttonDown = 0;         // the number of the pushbutton pin 0
 const int del = 5;                // servo refresh delay
 const int sup = 180;              // shift up degrees
 const int dow = 0;                // shift down degree
@@ -31,7 +38,7 @@ int pos = dow;                    // variable for servo position
 int gear = 0;                     // variable for gear position
 
 void setup() {
-  servo.attach(0);                // Set servo to digital pin 0
+  myservo.attach(SERVO_PIN);  // attaches the servo on pin 3 to the servo 
   
   // initialize the pushbutton pin as an input:
   pinMode(buttonUp, INPUT);
@@ -103,15 +110,15 @@ void loop() {                     // Loop through motion tests
 void up() {                       
    for(pos = mid; pos <= sup; pos += 1)      // goes from 90 degrees to 180 degrees 
      {                                       // in steps of 1 degree 
-       servo.write(pos);                     // tell servo to go to position in variable 'pos' 
-       delay(del);                      
-       SoftwareServo::refresh();             // required by SoftwareServo Library to sweep correctly
+       myservo.write(pos);                     // tell servo to go to position in variable 'pos' 
+       delay(REFRESH_PERIOD_MS);        // waits 20ms for refresh period 
+       SoftRcPulseOut::refresh(NOW);    // generates the servo pulse Now
      } 
    for(pos = sup; pos >= mid; pos -= 1)      // goes from 180 degrees to 90 degrees 
      {                                       // in steps of 1 degree 
-       servo.write(pos);                     // tell servo to go to position in variable 'pos' 
-       delay(del);                      
-       SoftwareServo::refresh();             // required by SoftwareServo Library to sweep correctly
+       myservo.write(pos);                     // tell servo to go to position in variable 'pos' 
+       delay(REFRESH_PERIOD_MS);        // waits 20ms for refresh period 
+       SoftRcPulseOut::refresh(NOW);    // generates the servo pulse Now
      }           
 }
 
@@ -119,40 +126,40 @@ void up() {
 void down() {                       
    for(pos = mid; pos>=dow; pos-=1)  
      {                                
-       servo.write(pos);                     // tell servo to go to position in variable 'pos' 
-       delay(del);                      
-       SoftwareServo::refresh();             // required by SoftwareServo Library to sweep correctly
+       myservo.write(pos);                     // tell servo to go to position in variable 'pos' 
+       delay(REFRESH_PERIOD_MS);        // waits 20ms for refresh period 
+       SoftRcPulseOut::refresh(NOW);    // generates the servo pulse Now
      }      
    for(pos = dow; pos<=mid; pos+=1)  
      {                                
-       servo.write(pos);                     // tell servo to go to position in variable 'pos' 
-       delay(del);                      
-       SoftwareServo::refresh();             // required by SoftwareServo Library to sweep correctly
+       myservo.write(pos);                     // tell servo to go to position in variable 'pos' 
+       delay(REFRESH_PERIOD_MS);        // waits 20ms for refresh period 
+       SoftRcPulseOut::refresh(NOW);    // generates the servo pulse Now
      }
   } 
 
 void centre() {
    for(pos = dow; pos <= mid; pos += 1)    // goes from 90 degrees to 0 degrees 
      {                                     // in steps of 1 degree 
-       servo.write(pos);                   // tell servo to go to position in variable 'pos' 
-       delay(del);                      
-       SoftwareServo::refresh();           // required by SoftwareServo Library to sweep correctly
+       myservo.write(pos);                     // tell servo to go to position in variable 'pos' 
+       delay(REFRESH_PERIOD_MS);        // waits 20ms for refresh period 
+       SoftRcPulseOut::refresh(NOW);    // generates the servo pulse Now
      }           
   }
     
 void netrual() {
    for(pos = mid; pos <= net; pos += 1)        // goes from 90 degrees to 135 degrees 
      {                                         // in steps of 1 degree 
-       servo.write(pos);                       // tell servo to go to position in variable 'pos' 
-       delay(del);                      
-       SoftwareServo::refresh();               // required by SoftwareServo Library to sweep correctly
+       myservo.write(pos);                     // tell servo to go to position in variable 'pos' 
+       delay(REFRESH_PERIOD_MS);        // waits 20ms for refresh period 
+       SoftRcPulseOut::refresh(NOW);    // generates the servo pulse Now
      }           
      delay (500);
    for(pos = net ; pos >= mid; pos -= 1)       // goes from 135 degrees to 90 degrees 
      {                                         // in steps of 1 degree 
-       servo.write(pos);                       // tell servo to go to position in variable 'pos' 
-       delay(del);                      
-       SoftwareServo::refresh();               // required by SoftwareServo Library to sweep correctly
+       myservo.write(pos);                     // tell servo to go to position in variable 'pos' 
+       delay(REFRESH_PERIOD_MS);        // waits 20ms for refresh period 
+       SoftRcPulseOut::refresh(NOW);    // generates the servo pulse Now
      } 
      gear = 0;
    for(int i=0; i<7 ; i +=1)
